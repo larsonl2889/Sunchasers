fbd5dad858f16d6000a55b20f9a14392495201b6
larsonl2889 <leifkormaklarson@gmail.com>
2024-09-30 23:15:32 -0500

    Direction, partial implementation
    Directions are intended to aid in the creation of pieces, and for interfacing between the float-based rotations in Unity and the Vector2-based calculations we'll be doing for connection logic.

---
+++ Assets/Scripts/Connection Logic/Connector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+
+public class Connector
+{
+    /// <summary>
+    /// The block object this connector represents.
+    /// </summary>
+    private Block block;
+    /// <summary>
+    /// Coordinates within the build area. Each coordinate should be an integer. 
+    /// </summary>
+    private Vector2 location;
+    /// <summary>
+    /// Array of relative coordinates that this connector will try to connect to. Each vector should be an orthogonal unit vector, i.e., Vector2(-1, 0).
+    /// </summary>
+    private Vector2[] allConnections;
+    /// <summary>
+    /// The build area this connector is located in.
+    /// </summary>
+    private BuildArea buildArea;
+
+/*    /// <summary>
+    /// As a standard,the left-most connection on the connector is facing.
+    /// </summary>
+    private float direction;*/
+
+    /// <summary>
+    /// constructor overload. See main constructor for details.
+    /// </summary>
+    /// <param name="block">Block obj this connector represents.</param>
+    /// <param name="location">Coordinate location within the build area. Should be an integer vector.</param>
+    /// <param name="allConnections">Relative locations this connector will try to connect to. This is intended to be an orthogonal unit vector.</param>
+    /// <param name="buildArea">The build area that this connector is a part of.</param>
+/*    /// <param name="angle"></param>*/
+    public Connector(Block block, Vector2 location, Vector2[] allConnections, BuildArea buildArea, Direction angle)
+    {
+        Connector(block, location, allConnections, buildArea, (float)angle);
+    }
+    /// <summary>
+    /// constructor
+    /// </summary>
+    /// <param name="block"></param>
+    /// <param name="location"></param>
+    /// <param name="allConnections"></param>
+    /// <param name="buildArea"></param>
+    /// <param name="angle"></param>
+    public Connector(Block block, Vector2 location, Vector2[] allConnections, BuildArea buildArea, float angle)
+	{
+
+	}
+}
---
+++ Assets/Scripts/Connection Logic/RotationOperator.cs
@@ -0,0 +1,8 @@
+﻿/*using System;
+using UnityEngine;
+
+public static class RotationOperator
+{
+	public Vector2 applyRotation(Vector2 subject, Direction dir) { }
+}
+*/
