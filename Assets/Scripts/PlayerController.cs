9420bc3ff343a711e02b84a7b8e434a40ab50c5c
G-ROUNDED <williamaugustine@myyahoo.com>
2024-10-02 15:16:57 -0500

    fixed stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,29 +13,43 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float jumpForce;
     private Rigidbody2D rb;
     private Vector2 direction;
+    private PlayerInput playerInput;
     
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerInput = new PlayerInput();
+        
     }
-    
-    private void FixedUpdate()
+    private void OnEnable()
     {
+        playerInput.Enable();
+    }
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    private void Update()
+    {
+        getInput();
         rb.velocity = new Vector2(direction.x * speed, rb.velocity.y);
     }
-
-    public void OnMove(InputAction.CallbackContext context)
+    public void getInput()
     {
-        direction = context.ReadValue<Vector2>();
+        if (playerInput.Player.Jump.triggered)
+        {
+            Jump();
+        }
+        direction = playerInput.Player.Move.ReadValue<Vector2>();
     }
-
-    public void OnJump(InputAction.CallbackContext context)
+    public void Jump()
     {
         if (IsGrounded() == true) {
             rb.AddForce(new Vector2(rb.velocity.x, jumpForce), ForceMode2D.Impulse);
         }
     }
-    bool IsGrounded()
+    public bool IsGrounded()
     {
         return rb.velocity.y == 0;
     }
