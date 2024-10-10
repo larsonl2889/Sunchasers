dcb268892f51d76c10f557a8dcfc873ec79967b4
larsonl2889 <larsonl2889@my.uwstout.edu>
2024-10-09 22:17:38 -0500

    SteamState, implemented
    not tested.

---
+++ Assets/Scripts/Connection Logic/SteamState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// Contributors: Leif Larson
+// Last updated 10/9/2024
+
+namespace Pipes
+{
+    public enum SteamState : byte
+    {
+        SOURCE = 2, FULL = 1, EMPTY = 0, LEAKING = -1
+    }
+    /// <summary>
+    /// Extension class for SteamState.
+    /// <br></br>Functionally, can be treated as holding "methods" for the SteamState enum.
+    /// </summary>
+    public static class SteamStateOps
+    {
+        public static bool IsOn(this SteamState state)
+        {
+            return state > 0;
+        }
+        public static bool IsSteaming(this SteamState state)
+        {
+            return state != 0;
+        }
+    }
+}
