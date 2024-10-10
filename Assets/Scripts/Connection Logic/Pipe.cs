65ffa9fe8be7883b5ea106f77648117d17f1c3c9
larsonl2889 <larsonl2889@my.uwstout.edu>
2024-10-09 22:17:51 -0500

    Pipe, created

---
+++ Assets/Scripts/Connection Logic/Pipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Blocks;
+using Cells;
+using Connectors;
+
+// Contributors: Leif Larson
+// Last updated 10/9/2024
+
+namespace Pipes
+{
+
+    public class Pipe : Block
+    {
+        private SteamState steamState;
+        private Connector pipeConnector;
+
+        public Pipe(Connector pipeConnector, bool isSource=false)
+            : base() // TODO put the parameters for Block in here!
+        {
+            // TODO implement!
+        }
+    }
+}
+
