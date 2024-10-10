a59fc89c3ded0a1ec49d3d57c3c99c0f0c6ab907
larsonl2889 <larsonl2889@my.uwstout.edu>
2024-10-09 21:54:27 -0500

    Connector, updated
    Functionality reduced, offloaded and simplified for pipe.

--- Assets/Scripts/Connection Logic/Connector_leif_10-1-24.cs
+++ Assets/Scripts/Connection Logic/Connector_leif_10-1-24.cs
@@ -2,26 +2,19 @@
 using UnityEngine;
 using DirectionOps;
 
+// Created by Leif Larson
+// last updated 10/9/2024
+
 namespace Connectors
 {
     public class Connector
     {
         private Vector2[] allLinks;
-        private Direction dir;
-        private Vector2 absPos;
-        public Vector2 parentPartPosition; // TODO this is debug and NOT actually stored here!
-
-        public Connector(Vector2[] allLinks, Direction dir)
+        
+        public Connector(Vector2[] allLinks)
         {
             this.allLinks = allLinks;
-            this.dir = dir;
         }
-
-        /// <summary>
-        /// Returns the direction.
-        /// </summary>
-        /// <returns>current direction</returns>
-        public Direction GetDirection() { return dir; }
 
         public Vector2[] GetAllLinks() { return allLinks; }
 
@@ -35,23 +28,7 @@ namespace Connectors
             {
                 allLinks[i] = rotation.ApplyRotation(allLinks[i]);
             }
-            dir = dir.Add(rotation);
         }
-
-        // THIS IS A STAND IN FOR GetBlock().GetParentPart().GetPos() used for testing.
-        public Vector2 GetParentPartPos()
-        {
-            return parentPartPosition;
-        }
-
-        public void Pivot(Direction rotation)
-        {
-            Vector2 deltaPos = absPos - GetParentPartPos();
-            deltaPos = rotation.ApplyRotation(deltaPos);
-            absPos = deltaPos + GetParentPartPos();
-            ApplyRotation(rotation);
-        }
-
 
     }
 }
