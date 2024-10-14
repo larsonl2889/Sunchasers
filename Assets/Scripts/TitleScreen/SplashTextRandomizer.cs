e6f6c10d60fb0494ef8a898b8ddaeefa806df1c6
AKP09YT <aidanmusil@gmail.com>
2024-10-14 16:27:16 -0500

    .

---
+++ Assets/Scripts/TitleScreen/SplashTextRandomizer.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashTextRandomizer : MonoBehaviour
+{
+    public static int Range(int minInclusive, int maxExclusive);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Range == 0)
+        {
+
+        }
+        else if (Range == 1)
+        {
+
+        }
+        else if (Range == 2)
+        {
+
+        }
+        else if (Range == 3)
+        {
+
+        }
+        else if (Range == 4)
+        {
+
+        }
+        else if (Range == 5)
+        {
+
+        }
+        else if (Range == 6)
+        {
+
+        }
+        else if (Range == 7)
+        {
+
+        }
+        else if (Range == 8)
+        {
+
+        }
+        else
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
