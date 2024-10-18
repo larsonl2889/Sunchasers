94032ddf54faf64de1d1c4d4aa30fb0004a91398
Kilian-W <kilianwilinski@gmail.com>
2024-10-17 21:56:33 -0500

    Grid Building Stuff

--- Assets/Scripts/BuildingArea_Riley.cs
+++ Assets/Scripts/BuildingArea_Riley.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Parts;
 using UnityEngine.UIElements;
 
 public class BuildingArea_Riley : MonoBehaviour
 {
     public GameObject Slot;
+    internal BuildAreaTest buildArea;
+    internal BuildMat buildMat;
+    internal Part part;
     private bool isInRange;
     private Vector2 position;
     private Vector2 WorldPos;
@@ -16,6 +20,9 @@ public class BuildingArea_Riley : MonoBehaviour
     void Start()
     {
         SlotHolder = new Stack<GameObject>();
+        buildArea = gameObject.GetComponentInParent<BuildAreaTest>();
+        buildMat = gameObject.GetComponent<BuildMat>();
+        part = Slot.GetComponent<Part>();
     }
 
     // Update is called once per frame
@@ -29,8 +36,21 @@ public class BuildingArea_Riley : MonoBehaviour
         //set the area that can be built in by reading the mouse position.
         position = Mouse.current.position.ReadValue();
         WorldPos = Camera.main.ScreenToWorldPoint(position);
-        Vector2 Spawnplace = new Vector2(WorldPos.x, WorldPos.y);
-        SlotHolder.Push(Instantiate(Slot, Spawnplace, Slot.transform.rotation));
+        int xPos = (int)WorldPos.x;
+        int yPos = (int)WorldPos.y;
+        float minX = buildMat.xPos - ((float)buildArea.scale / 2);
+        float minY = buildMat.yPos - ((float)buildArea.scale / 2);
+        float maxX = buildMat.xPos + ((float)buildArea.scale / 2);
+        float maxY = buildMat.yPos + ((float)buildArea.scale / 2);
+        if (xPos >= minX && xPos < maxX && yPos >= minY && yPos < maxY)
+        {
+            if(buildArea.CanMerge(part, new Vector2(xPos, yPos)))
+            {
+                Vector2 Spawnplace = new Vector2(xPos + 0.5f, yPos + 0.5f);
+                SlotHolder.Push(Instantiate(Slot, Spawnplace, Slot.transform.rotation));
+            }
+            
+        }
         
         
         //Instantiate(AnimalPrefabs[AnimalIndex], spawnPos, AnimalPrefabs[AnimalIndex].transform.rotation);
