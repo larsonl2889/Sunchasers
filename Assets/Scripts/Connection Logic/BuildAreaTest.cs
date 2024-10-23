7454700469c78e6482cc6c048e6dbb3a07f8375d
larsonl2889 <leifkormaklarson@gmail.com>
2024-10-23 15:14:07 -0500

    removed isLegal

--- Assets/Scripts/Connection Logic/BuildAreaTest.cs
+++ Assets/Scripts/Connection Logic/BuildAreaTest.cs
@@ -13,7 +13,6 @@ public class BuildAreaTest : MonoBehaviour
     public int scale = 1;
     public int xPos;
     public int yPos;
-    private bool isLegal = true;
 
     //Gives you manual control to place cells. (Good for setting up tests)
     public void placeCellManual(Cell cell, Vector2 pos)
