f85d5a302897a32ef5410f6022bb70d82bdbba70
Riley14-coding <rileygaetz@gmail.com>
2024-10-23 16:31:04 -0500

    Starts of hot Bar

---
+++ Assets/Scripts/HotBar.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotBar : MonoBehaviour
+{
+    internal BuildingArea_Riley builder;
+   public GameObject[] bar = new GameObject[9];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    //void PlaceBar(GameObject[] bars)
+    //{
+    //    BarIndex = 0;
+    //    //if(interact)
+    //    //build index
+    //    //move to next index
+    //    builder.Build(bars);
+    //}
+}
