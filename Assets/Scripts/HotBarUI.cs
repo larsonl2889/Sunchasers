f37981b77c99dc19b3648b21b1cb86114399cac3
AKP09YT <aidanmusil@gmail.com>
2024-11-06 16:28:06 -0600

    AHHH

--- Assets/Scripts/HotBarUI.cs
+++ Assets/Scripts/HotBarUI.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 
 public class HotBarUI : MonoBehaviour
 {
     private int UIindex = new int();
-    private GameObject UISlot;
-    internal HotBar hotbar;
+    private GameObject hotbarUISlot;        // 
+    internal HotBar hotbar;                 // Makes an objec to hotbar
+    public TextMeshProUGUI hotbarUINum;     // Hotbar number UI
+    public GameObject[] hotbarSlots;        // Creates an array of all hotbar slots
+    public GameObject slot;                 // Slot in UI
+    public GameObject UIParent;             // UI Parent (Hotbar)
+    //public GameObject hotbar;
+
     // Start is called before the first frame update
     void Start()
     {
+        GameObject instantiated = Instantiate(slot, transform.position, transform.rotation) as GameObject;
+        instantiated.transform.SetParent(UIParent.transform, false);
+        // Attempts to put the image of the item into the slot
         for (int i = 0; i < 9; i++)
         {
-            UISlot = hotbar.bar[i];
-
+            hotbarUISlot = hotbar.bar[i];
         }
     }
 
@@ -25,16 +34,13 @@ public class HotBarUI : MonoBehaviour
     }
     void SetUISlot(int index)
     {
-        UIindex=index;
-        UISlot = hotbar.bar[hotbar.index];
+        UIindex=index; //sets the index to be the same as the index of hotbar
+        hotbarUISlot = hotbar.bar[hotbar.index]; //sets the object to the index
+        //hotbarUINum
     }
     void SetSlots()
     {
 
     }
-    //public GameObject slot;
-    //public GameObject UIParent;
-    //
-    //GameObject instantiated = Instantiate(slot, transform.position, transform.rotation) as GameObject;
-    //instantiated.transform.SetParent(UIParent.transform, false);
+    
 }
