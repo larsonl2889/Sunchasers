acc5ba79d9bb5de5ed11a04554bcc2132fafc24a
G-ROUNDED <williamaugustine@myyahoo.com>
2024-11-06 22:50:10 -0600

    fixed hotbar bugs

--- Assets/Scripts/HotBarUI.cs
+++ Assets/Scripts/HotBarUI.cs
@@ -1,46 +1,69 @@
+
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
+
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class HotBarUI : MonoBehaviour
 {
-    private int UIindex = new int();
-    private GameObject hotbarUISlot;        // 
-    internal HotBar hotbar;                 // Makes an objec to hotbar
-    public TextMeshProUGUI hotbarUINum;     // Hotbar number UI
-    public GameObject[] hotbarSlots;        // Creates an array of all hotbar slots
-    public GameObject slot;                 // Slot in UI
-    public GameObject UIParent;             // UI Parent (Hotbar)
-    //public GameObject hotbar;
-
+    //private int[] UIindex = new int();
+    public HotBar hotbar;
+    public Sprite sprite;
+    public GameObject slot;
+    public GameObject UIParent;
+    public GameObject[] HotBarSlots = new GameObject[9];
+    public Sprite NewThing;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject instantiated = Instantiate(slot, transform.position, transform.rotation) as GameObject;
-        instantiated.transform.SetParent(UIParent.transform, false);
-        // Attempts to put the image of the item into the slot
-        for (int i = 0; i < 9; i++)
-        {
-            hotbarUISlot = hotbar.bar[i];
-        }
+        
     }
 
     // Update is called once per frame
+    public void updateImages()
+    {
+        if (hotbar != null)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                sprite = hotbar.bar[i].GetComponent<Image>().sprite;
+                HotBarSlots[i].gameObject.GetComponent<Image>().sprite = sprite;
+
+                //this.gameObject.GetComponent<Image>.sprite = NewThing;
+                //GameObject instantiated = Instantiate(slot, transform.position, transform.rotation) as GameObject;
+                //instantiated.transform.SetParent(UIParent.transform, false);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 9; i++)
+            {
+               
+                HotBarSlots[i].gameObject.GetComponent<Image>().sprite = null;
+
+                //this.gameObject.GetComponent<Image>.sprite = NewThing;
+                //GameObject instantiated = Instantiate(slot, transform.position, transform.rotation) as GameObject;
+                //instantiated.transform.SetParent(UIParent.transform, false);
+            }
+        }
+        
+    }
     void Update()
     {
         
     }
     void SetUISlot(int index)
     {
-        UIindex=index; //sets the index to be the same as the index of hotbar
-        hotbarUISlot = hotbar.bar[hotbar.index]; //sets the object to the index
-        //hotbarUINum
+        //UIindex=index;
+        //UISlot = hotbar.bar[hotbar.index];
     }
     void SetSlots()
     {
 
     }
-    
+
+
+
 }
