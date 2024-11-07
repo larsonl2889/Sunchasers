24a7941ccacdfe82f6e9ff812d6ff257c1e5ab52
larsonl2889 <leifkormaklarson@gmail.com>
2024-11-07 17:27:18 -0600

    Delegator, partial implementation

--- Assets/Scripts/BuildAreaDelegator.cs
+++ Assets/Scripts/BuildAreaDelegator.cs
@@ -7,7 +7,30 @@ using UnityEngine;
 public class BuildAreaDelegator : MonoBehaviour
 {
 
-    public Vector2Int objectiveLocation;
+    public bool objectiveIsOn;
+
+    public void UpdateObjective()
+    {
+        // If we need to turn off the objective
+        if (objectiveIsOn && !GetData().IsObjectiveOn()) {
+            DeactivateObjective();
+        }
+        // If we need to turn on the objective
+        else if (!objectiveIsOn && GetData().IsObjectiveOn())
+        {
+            ActivateObjective();
+        }
+    }
+
+    private void ActivateObjective()
+    {
+        // TODO
+    } 
+
+    private void DeactivateObjective()
+    {
+        // TODO
+    }
 
     protected BuildAreaTest GetData()
     {
