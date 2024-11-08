9f60ab4004d80146e50f0efe8a223b441af81cf0
larsonl2889 <leifkormaklarson@gmail.com>
2024-11-07 18:58:27 -0600

    SteamGusher, coded

---
+++ Assets/Scripts/Connection Logic/SteamGusher.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DirectionOps;
+
+public class SteamGusher : MonoBehaviour
+{
+    public ParticleSystem up;
+    public ParticleSystem left;
+    public ParticleSystem down;
+    public ParticleSystem right;
+
+    public void QuellAllGushers()
+    {
+        up.Stop();
+        left.Stop();
+        down.Stop();
+        right.Stop();
+    }
+
+    /// <summary>
+    /// Given a list of directions, update the gushers to match.
+    /// </summary>
+    /// <param name="dirs">List of directions</param>
+    public void UpdateGushers(List<Direction> dirs)
+    {
+        // UP
+        if (dirs.Contains(Direction.UP))
+        {
+            up.Play();
+        }
+        else 
+        { 
+            up.Stop(); 
+        }
+        // LEFT
+        if (dirs.Contains(Direction.LEFT))
+        {
+            left.Play();
+        }
+        else
+        {
+            left.Stop();
+        }
+        // DOWN
+        if (dirs.Contains(Direction.DOWN))
+        {
+            down.Play();
+        }
+        else
+        {
+            down.Stop();
+        }
+        // RIGHT
+        if (dirs.Contains(Direction.RIGHT))
+        {
+            right.Play();
+        }
+        else
+        {
+            right.Stop();
+        }
+    }
+}
