6887019904b3db290f77a5862b0c499155ab1384
AKP09YT <aidanmusil@gmail.com>
2024-12-02 15:47:32 -0600

    Fixed some issues with loading screen

--- Assets/Scripts/EndScreen/CreditsScroll.cs
+++ Assets/Scripts/EndScreen/CreditsScroll.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class CreditsScroll : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class CreditsScroll : MonoBehaviour {
+    public TextMeshProUGUI credits;
+    public void Start()
     {
-        
+        StartCoroutine(FadeOut());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator FadeOut()
     {
-        
+        float duration = 2f; //Fade out over 2 seconds.
+        float currentTime = 0f;
+        while (currentTime < duration)
+        {
+            float alpha = Mathf.Lerp(0f, 1f, currentTime / duration);
+            credits.color = new Color(credits.color.r, credits.color.g, credits.color.b, alpha);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        yield break;
     }
 }
